1db9ecec9dbd8dba68e6d57355a057ff29dcec6a
ts092000 <ts092000@gmail.com>
2020-12-01 14:09:13 +0700

    update DB

--- workboard.sql
+++ workboard.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Máy chủ: 127.0.0.1
--- Thời gian đã tạo: Th12 01, 2020 lúc 05:30 AM
+-- Thời gian đã tạo: Th12 01, 2020 lúc 08:07 AM
 -- Phiên bản máy phục vụ: 10.4.14-MariaDB
 -- Phiên bản PHP: 7.4.10
 
@@ -20,6 +20,27 @@ SET time_zone = "+00:00";
 --
 -- Cơ sở dữ liệu: `workboard`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Cấu trúc bảng cho bảng `task`
+--
+
+CREATE TABLE `task` (
+  `id` int(11) NOT NULL,
+  `id_task` int(11) NOT NULL,
+  `title` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL,
+  `description` mediumtext COLLATE utf8_vietnamese_ci NOT NULL,
+  `comment` varchar(255) COLLATE utf8_vietnamese_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_vietnamese_ci;
+
+--
+-- Đang đổ dữ liệu cho bảng `task`
+--
+
+INSERT INTO `task` (`id`, `id_task`, `title`, `description`, `comment`) VALUES
+(1, 0, 'DO', '123', '12345');
 
 -- --------------------------------------------------------
 
@@ -44,6 +65,12 @@ INSERT INTO `to_do_list` (`id`, `name`) VALUES
 --
 
 --
+-- Chỉ mục cho bảng `task`
+--
+ALTER TABLE `task`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Chỉ mục cho bảng `to_do_list`
 --
 ALTER TABLE `to_do_list`
@@ -52,6 +79,12 @@ ALTER TABLE `to_do_list`
 --
 -- AUTO_INCREMENT cho các bảng đã đổ
 --
+
+--
+-- AUTO_INCREMENT cho bảng `task`
+--
+ALTER TABLE `task`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT cho bảng `to_do_list`
