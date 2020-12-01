e08a15085f23b81bb9d8b05787660682e8e6909b
ts092000 <ts092000@gmail.com>
2020-12-01 11:11:00 +0700

    db

---
+++ Todo.sql
@@ -0,0 +1,20 @@
+create database app
+use app
+create table ToDoList(
+Name nvarchar(255)
+)
+create table Task(
+Tittle nvarchar(255),
+Description nvarchar (255),
+Comment varchar(255)
+)
+select*from ToDoList
+select*from Task
+
+insert into ToDoList values ()
+insert into ToDoList values ()
+insert into ToDoList values ()
+insert into Task values ()
+insert into Task values ()
+insert into Task values ()
+
